cf3c4e43845ae0368bbebee5bb2e3c6e083db289
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-02-24 11:11:10 +0530

    connected to the database

--- src/main/java/com/group11/App.java
+++ src/main/java/com/group11/App.java
@@ -1,5 +1,7 @@
 package com.group11;
 
+import com.group11.config.MysqlConnect;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MysqlConnect.ConnectDB();
     }
 }
---
+++ src/main/java/com/group11/config/MysqlConnect.java
@@ -0,0 +1,24 @@
+package com.group11.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class MysqlConnect {
+    private static final String URL = "jdbc:mysql://localhost:3306/schoolManagementSystem";
+    private static final String DATABASE_USER_NAME = "root";
+    private static final String DATABASE_PASSWORD = "123@Ishan";
+    Connection con = null;
+   
+    public static Connection ConnectDB(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(URL, DATABASE_USER_NAME, DATABASE_PASSWORD);
+            JOptionPane.showMessageDialog(null, "Connected to Database");
+            return con;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
