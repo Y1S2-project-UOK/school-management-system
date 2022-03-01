be10b51ee4154ca38b3d9c1bf2f2257eac507c45
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-03-01 11:43:43 +0530

    added positin field to sql script

--- SqlScript/loginTable.sql
+++ SqlScript/loginTable.sql
@@ -3,19 +3,20 @@ create database schoolManagementSystem;
 use schoolManagementSystem;
 
 create table loginUserDetails(
-Email varchar(50) primary key,
 fullName varchar(50) Not null,
+Email varchar(50) primary key,
 loginPassword varchar(16) not null,
+position varchar(16) not null,
 reasonToJoin Varchar(255) not null,
 aproveStatus int default 0,
 check(-1 < aproveStatus and  aproveStatus < 2)
 ) ;
 
 insert into loginUserDetails values(
-"kamal@gmail.com","Kamal",4321,"teacher",1
+"full name","email@gmail.com","123@ishan","teacher","check my result",1
 );
 
--- delete from loginUserDetails where Email = "kamal@gmail.com";
+-- delete from loginUserDetails where Email = "email@gmail.com";
 
 -- drop table loginUserDetails;
 
