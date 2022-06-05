afa3437aa2e9f2006f41d32803554ad6de3dfeb4
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-06-05 07:34:10 +0530

    move staff package to correct place

--- src/main/java/com/group11/staff/StaffDetails.java
+++ src/main/java/com/group11/staff/StaffDetails.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package staff;
+package com.group11.staff;
 
 /**
  *
