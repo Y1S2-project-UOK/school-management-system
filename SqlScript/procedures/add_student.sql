394fd5a5c81c621a763ec16cf39e047cdca5e2c5
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-06-08 12:46:33 +0530

    Add new teacher function implemented

---
+++ SqlScript/procedures/add_student.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE add_student(student_admission_no varchar(50), student_name varchar(255), student_email varchar(255), student_date_of_birth date, gender varchar(20), student_address varchar(255), student_nationality varchar(255), student_religion varchar(255), student_password varchar(255), guradian_id varchar(50)) 
+BEGIN
+  INSERT INTO student (student_admission_no, gender, student_address, student_date_of_birth, student_email, student_name, student_nationality, student_password, student_religion, guardian_id)
+  VALUES(student_admission_no, gender, student_address, student_date_of_birth, student_email, student_name, student_nationality, student_password, student_religion, guardian_id);
+END $$ 
+DELIMITER ;
+
+-- CALL add_student();
+
+
---
+++ src/main/java/com/group11/Student/db/Student.java
@@ -0,0 +1,187 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group11.Student.db;
+
+import com.group11.config.MysqlConnect;
+import com.group11.staff.db.Staff;
+import com.mysql.cj.jdbc.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hp
+ */
+public class Student {
+    
+    private String studentRegNo;
+    private String studentName;
+    private String studentEmail;
+    private String studentDOB;
+    private String studentGender;
+    private String studentAddress;
+    private String studentNationality;
+    private String studentReligion;
+    private String studentPassword;
+    private String studentGuardianID;
+
+    public Student(String studentRegNo, String studentName, String studentEmail, String studentDOB, String studentGender, String studentAddress, String studentNationality, String studentReligion, String studentPassword, String studentGuardianID) {
+        this.studentRegNo = studentRegNo;
+        this.studentName = studentName;
+        this.studentEmail = studentEmail;
+        this.studentDOB = studentDOB;
+        this.studentGender = studentGender;
+        this.studentAddress = studentAddress;
+        this.studentNationality = studentNationality;
+        this.studentReligion = studentReligion;
+        this.studentPassword = studentPassword;
+        this.studentGuardianID = studentGuardianID;
+    }
+
+    public String getStudentRegNo() {
+        return studentRegNo;
+    }
+
+    public void setStudentRegNo(String studentRegNo) {
+        this.studentRegNo = studentRegNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentDOB() {
+        return studentDOB;
+    }
+
+    public void setStudentDOB(String studentDOB) {
+        this.studentDOB = studentDOB;
+    }
+
+    public String getStudentGender() {
+        return studentGender;
+    }
+
+    public void setStudentGender(String studentGender) {
+        this.studentGender = studentGender;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public String getStudentNationality() {
+        return studentNationality;
+    }
+
+    public void setStudentNationality(String studentNationality) {
+        this.studentNationality = studentNationality;
+    }
+
+    public String getStudentReligion() {
+        return studentReligion;
+    }
+
+    public void setStudentReligion(String studentReligion) {
+        this.studentReligion = studentReligion;
+    }
+
+    public String getStudentPassword() {
+        return studentPassword;
+    }
+
+    public void setStudentPassword(String studentPassword) {
+        this.studentPassword = studentPassword;
+    }
+
+    public String getStudentGuardianID() {
+        return studentGuardianID;
+    }
+
+    public void setStudentGuardianID(String studentGuardianID) {
+        this.studentGuardianID = studentGuardianID;
+    }
+    
+    public static ArrayList<Student> getStudentList() {
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        try {
+            Connection conn = MysqlConnect.ConnectDB();
+
+            String query = "CALL get_Student_table();";
+            CallableStatement stmt = (CallableStatement) conn.prepareCall(query);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Student student = new Student(rs.getString("student_admission_no"), rs.getString("gender"),
+                        rs.getString("student_address"), rs.getString("student_date_of_birth"), rs.getString("student_email"),
+                        rs.getString("student_name"), rs.getString("student_nationality"), rs.getString("student_password"),
+                        rs.getString("student_religion"), rs.getString("guardian_id"));
+                studentList.add(student);
+            }
+            return studentList;
+
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, e.getMessage(), e.getMessage(), 0);
+            return null;
+        }
+    }
+    
+    public static boolean addStudent(
+            String studentNo,
+            String gender,
+            String address,
+            String dob,
+            String email,
+            String name,
+            String nationality,          
+            String pwd,
+            String religion,
+            String guardianID
+            ) {
+        try {
+            Connection conn = MysqlConnect.ConnectDB();
+            // validate staff member
+            String query = "CALL add_student(?,?,?,?,?,?,?,?,?,?);";
+            CallableStatement stmt = (CallableStatement) conn.prepareCall(query);
+            stmt.setString(1, studentNo);
+            stmt.setString(2, gender);
+            stmt.setString(3, address);
+            stmt.setString(4, dob);
+            stmt.setString(5, email);
+            stmt.setString(6, name);
+            stmt.setString(7, nationality);
+            stmt.setString(8, pwd);
+            stmt.setString(9, religion);
+            stmt.setString(10, guardianID);
+            System.out.println(stmt.execute());
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "", 1);
+            return false;
+        }
+    }
+    
+}
