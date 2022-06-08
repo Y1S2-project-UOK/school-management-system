cdad1d98c268cddb6a79bae360026c937eb6df68
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-06-08 06:01:48 +0530

    move package to correct place

--- src/main/java/com/group11/Student/StudentDetails.java
+++ src/main/java/com/group11/Student/StudentDetails.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Student;
+package com.group11.Student;
 
 /**
  *
